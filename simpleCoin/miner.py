550a8f16d41915e3c36c0374662a916a23d301a7
JewishRateCoin <162920952+JewishRateCoin@users.noreply.github.com>
2024-03-10 16:29:10 -0400

    Update miner.py

--- simpleCoin/miner.py
+++ simpleCoin/miner.py
@@ -250,7 +250,7 @@ def validate_signature(public_key, signature, message):
 
 def welcome_msg():
     print("""       =========================================\n
-        SIMPLE COIN v1.0.0 - BLOCKCHAIN SYSTEM\n
+        JEWISHRATECOIN v1.0.0 - BLOCKCHAIN SYSTEM\n
        =========================================\n\n
         You can find more help at: https://github.com/cosme12/SimpleCoin\n
         Make sure you are using the latest version or you may end in
